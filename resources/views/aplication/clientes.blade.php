b8f20e8f47551e072bf7e51856c6c32ec98f97bd
alracafi <alracafi@gmail.com>
2021-08-10 20:48:15 +0100

    ocultar botones

--- resources/views/aplication/clientes.blade.php
+++ resources/views/aplication/clientes.blade.php
@@ -18,6 +18,7 @@
             </div>
             <div class="collapse navbar-collapse">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    {{-- ------------------BOTON AÑADIR----------------------------------------------------- --}}
 
                     {{---------------------boton añadir solo disponible cuando haya localidad y zona en la BD --}}
 
@@ -26,51 +27,53 @@
                         <a class="form-control me-2 border border-danger bg-green-400"
                             href="{{ route('clientes.create') }}">{{__('buttons.add_client')}}</a>
                         @else
-                        <button id="btn_add" class="form-control me-2 border border-danger bg-green-400"
-                            >{{__('buttons.add_client')}}</button>
+                        <button id="btn_add"
+                            class="form-control me-2 border border-danger bg-green-400">{{__('buttons.add_client')}}</button>
                         @endif
 
                     </li>
+                    {{--------------------------------BOTON LOCALIDADES/ZONAS------------------------------------------ --}}
+
                     <li class="nav-item">
                         <a class="form-control me-2 border border-danger bg-gray-300"
                             href="{{route('localidades.index')}}">{{__('buttons.config')}}</a>
                     </li>
+                    {{-- ---------------------------------BOTON GUARDAR PDF-------------------------------------------------- --}}
+
+                    @if ($num>0)
+
                     <li class="nav-item">
-                        @if ($num>0)
                         <a class="form-control me-2 border border-danger  bg-yellow-300""
                             href=" {{route('pdf', $cliente_sin_paginar->pluck('id'))}}">{{__('buttons.save_pdf')}}</a>
+                    </li>
+                    {{-------------------BOTON FILTRAR-----------------------------------------------------------------}}
+                    <li class=" nav-item">
+                        {{--------- comprueba que esta en modo filtrado o modo total para mostrar el boton filtrar o volver al total ------}}
 
+                        @if ($modo_filtrado)
+                        {{--------------------------------------------modo filtrado--------------------------------------------------------}}
+                        <a class="form-control me-2 bg-red-500"
+                            href="{{route('clientes.index')}}">{{__('buttons.exit_filter')}}</a>
                         @else
-                        <button class="form-control me-2 border border-danger disabled>{{__('buttons.save_pdf')}}</button>
+                        <a class="form-control me-2 border border-danger" href="#" data-bs-toggle="modal"
+                            data-bs-target="#filtros" id="botonMostrarFiltros">{{__('buttons.filter')}}</a>
                         @endif
                     </li>
-                   
-                    <li class=" nav-item">
-
-                            {{--------- comprueba que esta en modo filtrado o modo total para mostrar el boton filtrar o volver al total ------}}
-
-                            @if ($modo_filtrado)
-                            {{--------------------------------------------modo filtrado--------------------------------------------------------}}
-                            <a class="form-control me-2 bg-red-500"
-                                href="{{route('clientes.index')}}">{{__('buttons.exit_filter')}}</a>
-                            @else
-                            <a class="form-control me-2 border border-danger" href="#" data-bs-toggle="modal"
-                                data-bs-target="#filtros" id="botonMostrarFiltros">{{__('buttons.filter')}}</a>
-                            @endif
-                    </li>
+                    @endif
                 </ul>
                 <div class="navbar-nav me-auto mb-2 mb-lg-0">
                     {{-- --------------------------------PAGINADOR---------------------------------------------------------- --}}
-
+                    @if ($num>0)
                     {{------------------------------------------ORDENAR RESULTADOS------------------------------------------- --}}
                     @include('forms.orderform')
-                    {{-------------------------------------------------- LOGOUT--------------------------------------------- --}}
-                    @include('forms.logoutform')
+
                     {{----------------------------------------------BUSCADOR----------------------------------------------------------}}
                     @include('forms.searchform')
-                    @php
-                    echo(session('mensaje'));
-                    @endphp
+
+                    @endif
+
+                    {{-------------------------------------------------- LOGOUT--------------------------------------------- --}}
+                    @include('forms.logoutform')
                     {{-----------------------------------------------FIN BUSCADOR------------------------------------------------- --}}
                 </div>
             </div>
@@ -78,10 +81,12 @@
     </nav>
     {{-- -------------------------------------------------------fin nav--------------------------- --}}
 
+    @if ($num>0)
+
     <div class="col-md-2">
         @include('forms.paginaform')
     </div>
-    {{--  <div style="height: 12px"></div>--}}
+    @endif
 </div>
 
 {{-- -----------------------------------------FIN PARTE SUPERIOR TABLA--------------------------------------------- --}}
